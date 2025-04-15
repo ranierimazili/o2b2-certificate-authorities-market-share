282f5bcd7396511eb9408e75d4d142e23a6738c0
Ranieri Mazili <ranieri85@gmail.com>
2025-04-15 15:03:00 -0300

    Added the non-protected endpoints

--- README.md
+++ README.md
@@ -2,22 +2,65 @@
 Estimates the market share of certificate authorities of Open Finance Brasil
 
 ## Metodology
-1) Reads the [partipants url](https://data.directory.openbankingbrasil.org.br/participants) and extract all distinct hosts from declared APIs of payments-consents or consents family
+1) Reads the [partipants url](https://data.directory.openbankingbrasil.org.br/participants) and extract all distinct hosts from declared APIs of payments-consents or consents family, extract the client certificate from each host and show the results grouped by CA.
 
-2) Extract the client certificate from each host
-
+2) Reads the well-known of each authorization server, extract the client certificate of the authorization_endpoint and show the results grouped by CA.
 
 ## Most recent results
 
-Results in 23/02/2025
+Results in 15/04/2025
 ```json
+mTLS protected endpoints results: 
 {
-  total: 131,
-  'AC SOLUTI SSL EV G4': 99,
-  'AC Certisign ICP-Brasil SSL EV G4': 23,
-  'AC SERASA SSL EV V4': 5,
+  total: 133,
+  'AC Certisign ICP-Brasil SSL EV G4': 27,
+  'AC SERASA SSL EV V4': 2,
+  'AC SOLUTI SSL EV G4': 97,
+  'AC VALID SSL EV': 4,
   'Autoridade Certificadora do SERPRO SSLv1': 2,
-  'AC VALID SSL EV': 2
+  'Openway TLS': 1
+}
+non-protected endpoints results:
+{
+  total: 208,
+  'AC Certisign ICP-Brasil SSL EV G4': 1,
+  'AC SOLUTI SSL EV G4': 4,
+  'AC VALID SSL EV': 1,
+  'Amazon RSA 2048 M02': 51,
+  'Amazon RSA 2048 M03': 4,
+  'Autoridade Certificadora do SERPRO SSLv1': 1,
+  'COMODO RSA Extended Validation Secure Server CA': 1,
+  'DigiCert EV RSA CA G2': 7,
+  'DigiCert Global G2 TLS RSA SHA256 2020 CA1': 3,
+  'DigiCert SHA2 Extended Validation Server CA': 7,
+  'DigiCert TLS Hybrid ECC SHA384 2020 CA1': 1,
+  'DigiCert TLS RSA SHA256 2020 CA1': 12,
+  'E-SAFER DOMAIN SSL CA  [Run by the Issuer]': 1,
+  'E-SAFER EXTENDED SSL CA  [Run by the Issuer]': 8,
+  E5: 2,
+  E6: 2,
+  'Entrust Certification Authority - L1K': 1,
+  'GeoTrust EV RSA CA G2': 2,
+  'GeoTrust RSA CN CA G2': 1,
+  'GeoTrust TLS RSA CA G1': 3,
+  'GlobalSign Extended Validation CA - SHA256 - G3': 19,
+  'GlobalSign RSA OV SSL CA 2018': 6,
+  'Go Daddy Secure Certificate Authority - G2': 5,
+  'Openway TLS': 1,
+  R10: 7,
+  R11: 19,
+  'RapidSSL TLS RSA CA G1': 1,
+  'Sectigo RSA Extended Validation Secure Server CA': 1,
+  'Sectigo RSA Organization Validation Secure Server CA': 3,
+  'Site Blindado RSA Domain Validation Secure Server CA 3': 1,
+  'Soluti CA - DV': 2,
+  'Soluti CA - EV': 15,
+  'Thawte TLS RSA CA G1': 2,
+  'TrustSign BR RSA DV SSL CA 3': 1,
+  'Valid Certificadora RSA EV SSL CA': 5,
+  WE1: 3,
+  WR3: 2,
+  'ZeroSSL RSA Domain Secure Site CA': 2
 }
 ```
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 import tls from 'tls';
 import net from 'net';
 import { X509Certificate } from 'node:crypto'
@@ -31,10 +33,9 @@ const getCertificate = async function (host, port = 443) {
     });
 }
 
-const getMtlsEndpointsFromDirectory = async function(environment) {
-    const participants = await getParticipantsFromDirectory(environment);
+const getMtlsEndpointsFromDirectory = async function(participants) {
+    //const participants = await getParticipantsFromDirectory(environment);
     const mtlsHosts = [];
-    //const participants = temp_participants;
     for (const org of participants) {
         for (var i=0;i<org.AuthorisationServers.length;i++) {
             const as = org.AuthorisationServers[i];
@@ -56,6 +57,26 @@ const getMtlsEndpointsFromDirectory = async function(environment) {
     return [...new Set(mtlsHosts)];
 }
 
+const getAuthorizationEndpointsFromParticipants = async function(participants) {
+    //const participants = await getParticipantsFromDirectory(environment);
+    const authorizationEndpoints = [];
+    //const participants = temp_participants;
+    for (const org of participants) {
+        for (var i=0;i<org.AuthorisationServers.length;i++) {
+            try {
+                const res = await fetch(org.AuthorisationServers[i].OpenIDDiscoveryDocument);
+                const wellknown = await res.json();
+                const url = new URL(wellknown.authorization_endpoint);
+                authorizationEndpoints.push(url.host);
+                console.log(`Endpoint de autorização encontrado: ${wellknown.authorization_endpoint}`);
+            } catch (e) {
+                console.error("Erro ao tentar extrair o authorization_endpoint do endpoint: ", org.AuthorisationServers[i].OpenIDDiscoveryDocument);
+            }
+        }
+    }
+    return [...new Set(authorizationEndpoints)];
+}
+
 const getParticipantsFromDirectory = async function(environment) {
     const directoryParticipantsUrl = environment == "sandbox" ? "https://data.sandbox.directory.openbankingbrasil.org.br/participants" : "https://data.directory.openbankingbrasil.org.br/participants";
     const res = await fetch(directoryParticipantsUrl);
@@ -64,32 +85,71 @@ const getParticipantsFromDirectory = async function(environment) {
     return participants;
 }
 
-//create async function to start the job
+const sort = function(data) {
+    const { total, ...rest } = data;
+
+    const sorted = Object.fromEntries(
+      Object.entries(rest).sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+    );
+    
+    const final = { total, ...sorted };
+
+    return final;
+}
+
 const start = async function() {
-    let results = {
+    let resultsProtectedEndpoints = {
         'total': 0
     };
-    const mtlsHosts = await getMtlsEndpointsFromDirectory("production");
+    let resultsUnprotectedEndpoints = {
+        'total': 0
+    };
+
+    const participants = await getParticipantsFromDirectory("production");
+
+    const mtlsHosts = await getMtlsEndpointsFromDirectory(participants);
     for (const host of mtlsHosts) {
         try {
             const cert = await getCertificate(host);
-            if (results[cert.toLegacyObject().issuer.CN]) {
-                results[cert.toLegacyObject().issuer.CN]++;
-                results['total']++;
+            if (resultsProtectedEndpoints[cert.toLegacyObject().issuer.CN]) {
+                resultsProtectedEndpoints[cert.toLegacyObject().issuer.CN]++;
+                resultsProtectedEndpoints['total']++;
             } else {
-                results[cert.toLegacyObject().issuer.CN] = 1;
-                results['total']++;
+                resultsProtectedEndpoints[cert.toLegacyObject().issuer.CN] = 1;
+                resultsProtectedEndpoints['total']++;
             }
             
             console.log(`Certificado do host ${host} obtido com sucesso.`);
             console.log(cert.toLegacyObject().issuer.CN);
-            //break;
         } catch (e) {
             console.error(`Erro ao obter certificado do host ${host}: `, e);
         }
     }
-    return results;
+
+    const authorizationEndpoints = await getAuthorizationEndpointsFromParticipants(participants);
+    for (const host of authorizationEndpoints) {
+        try {
+            const cert = await getCertificate(host);
+            if (resultsUnprotectedEndpoints[cert.toLegacyObject().issuer.CN]) {
+                resultsUnprotectedEndpoints[cert.toLegacyObject().issuer.CN]++;
+                resultsUnprotectedEndpoints['total']++;
+            } else {
+                resultsUnprotectedEndpoints[cert.toLegacyObject().issuer.CN] = 1;
+                resultsUnprotectedEndpoints['total']++;
+            }
+            
+            console.log(`Certificado do host ${host} obtido com sucesso.`);
+            console.log(cert.toLegacyObject().issuer.CN);
+        } catch (e) {
+            console.error(`Erro ao obter certificado do host ${host}: `, e);
+        }
+    }
+
+    return [sort(resultsProtectedEndpoints), sort(resultsUnprotectedEndpoints)];
 }
 
 const results = await start();
-console.log(results);
+console.log("mTLS protected endpoints results: ");
+console.log(results[0]);
+console.log("non-protected endpoints results: ");
+console.log(results[1]);
